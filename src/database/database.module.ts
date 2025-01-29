ac4da623f6394ca2028ae81d6556a59b04c15b41
ezioeg <ezioeg@gmail.com>
2025-01-29 11:38:57 -0400

    Prueba
    descripcion prueba

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { Pool } from 'pg';
 
-// Aquí estamos creando un provider para la base de datos PostgreSQL
+// Aquí estamos creando un provider para la base de datos postgreSQL
 const dbProvider = {
   provide: 'DATABASE_POOL', // Esto es lo que vamos a inyectar en otros servicios
   useFactory: () => {
     return new Pool({
       host: 'localhost', // Dirección de tu base de datos
       port: 5432, // Puerto de PostgreSQL (puede variar)
-      user: 'ezioeg', // Usuario de PostgreSQL
-      password: '1307', // Contraseña de PostgreSQL
+      user: 'ezioeg', // Usuario de postgreSQL
+      password: '1307', // Contraseña de postgreSQL
       database: 'graphrestapi_nest_db', // Nombre de la base de datos
     });
   },
--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,19 +1,19 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { IsOptional, IsString, IsNumber } from 'class-validator';
 
-@InputType() // Necesario para GraphQL
+@InputType() // Necesario para graphQL
 export class UpdateProductDto {
-  @Field({ nullable: true }) // Necesario para GraphQL
+  @Field({ nullable: true }) // Necesario para graphQL
   @IsOptional()
   @IsString()
   name?: string;
 
-  @Field({ nullable: true }) // Necesario para GraphQL
+  @Field({ nullable: true }) // Necesario para graphQL
   @IsOptional()
   @IsNumber()
   price?: number;
 
-  @Field({ nullable: true }) // Necesario para GraphQL
+  @Field({ nullable: true }) // Necesario para graphQL
   @IsOptional()
   @IsString()
   description?: string;
